638ff6d3d1b1feaefbc1ac8b8b110d9d6f0dd7c7
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2025-04-04 20:23:25 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@
     dotenv.config();  // لتحميل المتغيرات البيئية
 
     const app = express();
- const port = process.env.PORT || 5000;
+ const port = process.env.PORT ;
 
     app.use(cors());
     app.use(express.json());
