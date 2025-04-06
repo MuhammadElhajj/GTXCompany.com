e148aa666ca03f04ce30f1913afd49ef3efdad64
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2025-04-06 14:04:42 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@
 
     const app = express();
  // const port = process.env.PORT || 4000;  
- const port = 'https://gtxcompany-backend-com.onrender.com' || 5000 ;  
+ const port = process.env.PORT || 5000;  
+ // const port = 'https://gtxcompany-backend-com.onrender.com' || 5000 ;  
 
     app.use(cors());
     app.use(express.json());
@@ -38,12 +39,12 @@
         // إرسال البريد الإلكتروني
         await transporter.sendMail(mailOptions);
         res.status(200).json({ message: 'Email sent successfully!' });
-          alert("Email sent successfully!");
+           console.log("Email sent successfully!");
     
         
       } catch (error) {
         res.status(500).json({ message: 'Error sending email', error: error.message });
-           alert("error : " + error.message );
+           console.log("error : " + error.message );
      
         
       }
